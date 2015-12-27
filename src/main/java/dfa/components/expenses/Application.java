3222f3afdfbf1f392025b1c16c44e8c09b762fe0
Barry O'Sullivan <o_sullivanbarry@hotmail.com>
2015-12-27 19:45:46 +0000

    Adding translation and login feature to application

--- src/main/java/dfa/components/expenses/Application.java
+++ src/main/java/dfa/components/expenses/Application.java
@@ -1,32 +1,57 @@
 package dfa.components.expenses;
 
+import java.io.IOException;
+import java.security.Principal;
 import java.util.Date;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.rest.webmvc.BaseUriAwareController;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
 @SpringBootApplication
+@BaseUriAwareController
 public class Application {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
+	
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
 	}
 
 	@Bean
-	public CommandLineRunner demo(ExpensesRepository repository) {
+	public CommandLineRunner demo(ExpensesRepository repository, UserRepository users) {
 		return (args) -> {
 			// save a couple of customers
 			repository.save(new Expense("Brownes Hardware", new Date(), false, new Date()));
-			/*repository.save(new Expense("Chloe", "O'Brian"));
-			repository.save(new Expense("Kim", "Bauer"));
-			repository.save(new Expense("David", "Palmer"));
-			repository.save(new Expense("Michelle", "Dessler"));*/
+			users.save(new User("user", "password"));
 
 			// fetch all customers
 			log.info("Customers found with findAll():");
@@ -49,11 +74,56 @@ public class Application {
 			for (Expense bauer : repository.findExpenseById(Long.parseLong("1"))) {
 				log.info(bauer.toString());
 			}
+			for (User user: users.findUserById(Long.parseLong("1"))) {
+				log.info(user.toString());
+			}
             log.info("");
 		};
 	}
 	
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.httpBasic().and().authorizeRequests()
+					.antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().anyRequest()
+					.authenticated().and().csrf()
+					.csrfTokenRepository(csrfTokenRepository()).and()
+					.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+		}
+
+		private Filter csrfHeaderFilter() {
+			return new OncePerRequestFilter() {
+				@Override
+				protected void doFilterInternal(HttpServletRequest request,
+						HttpServletResponse response, FilterChain filterChain)
+						throws ServletException, IOException {
+					CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
+							.getName());
+					if (csrf != null) {
+						Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+						String token = csrf.getToken();
+						if (cookie == null || token != null
+								&& !token.equals(cookie.getValue())) {
+							cookie = new Cookie("XSRF-TOKEN", token);
+							cookie.setPath("/");
+							response.addCookie(cookie);
+						}
+					}
+					filterChain.doFilter(request, response);
+				}
+			};
+		}
+
+		private CsrfTokenRepository csrfTokenRepository() {
+			HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+			repository.setHeaderName("X-XSRF-TOKEN");
+			return repository;
+		}
+	}
+	
 	// TESTIN WITH CURL
 	//curl -i -u user:password -X POST -H "Content-Type:application/json" -d '{"merchantName": "Kerry Co op", "dateOfPurchase" : "Frodo","paymentDueDate": "Baggins" }' http://localhost:8080/expenses
-
+	//curl -H "X-Requested-With: XMLHttpRequest" http://localhost:8080/user
 }
--- src/main/java/dfa/components/expenses/ExpensesRepository.java
+++ src/main/java/dfa/components/expenses/ExpensesRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.BaseUriAwareController;
 
+@BaseUriAwareController
 @RepositoryRestResource(collectionResourceRel = "expenses", path = "expenses")
 public interface ExpensesRepository extends CrudRepository<Expense, Long> {
 
